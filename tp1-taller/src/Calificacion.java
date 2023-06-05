8b4a325776894999d86f579a363b03757a028d71
Camilo Griffa <camilogriffa@protonmail.com>
2023-06-05 09:51:29 -0300

    actualizando repo

--- tp1-taller/src/Calificacion.java
+++ tp1-taller/src/Calificacion.java
@@ -1,12 +1,12 @@
 public class Calificacion {
     public static void main(String[] args) {
-        enum Notas { PERDIODERECHO, APROBADO, NOTABLE, SOBRESALIENTE, MATRICULA_DE_HONOR, NO_PRESENTADO };
-        public static Notas clasificacionNotas(float nota) {
-            String calificación = "";
-            if (nota < 3.0) {
-                calificación = String.valueOf(Notas.PERDIODERECHO);
-            } else if ()
+        // enum Notas { PERDIODERECHO, APROBADO, NOTABLE, SOBRESALIENTE, MATRICULA_DE_HONOR, NO_PRESENTADO };
+        // public static Notas clasificacionNotas(float nota) {
+        //     String calificación = "";
+        //     if (nota < 3.0) {
+        //         calificación = String.valueOf(Notas.PERDIODERECHO);
+        //     } else if ()
 
-        }
+        // }
     }
 }
